f11ce9237b88b479899a54b00e3bfd3aff90c416
seoliee <dev.seoliee49@gmail.com>
2026-04-21 13:01:49 +0900

    chore: 백엔드, 인프라 프로젝트 구조 설정

---
+++ BE/src/main/java/com/salmon/studion/StudionApplication.java
@@ -0,0 +1,13 @@
+package com.salmon.studion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudionApplication.class, args);
+	}
+
+}

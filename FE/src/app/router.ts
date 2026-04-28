a7746b40104781c8a9ea7362a0644bc8713595f2
joosb07 <joosb07@naver.com>
2026-04-28 12:31:19 +0900

    feat: 온보딩 페이지 및 로그인 버튼 구현

--- FE/src/app/router.ts
+++ FE/src/app/router.ts
@@ -1,13 +1,19 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import DashboardPage from '@/pages/Dashboard/DashboardPage.vue'
 import ProjectPage from '@/pages/Project/ProjectPage.vue'
+import OnboardingPage from '@/pages/Onboarding/OnboardingPage.vue'
 
 const router = createRouter({
   history: createWebHistory(),
   routes: [
     {
       path: '/',
-      redirect: '/dashboard',
+      redirect: '/onboarding',
+    },
+    {
+      path: '/onboarding',
+      name: 'onboarding',
+      component: OnboardingPage,
     },
     {
       path: '/dashboard',

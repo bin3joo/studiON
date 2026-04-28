747e4cfffc298997a3e7ffbba8765cae70e7997e
jeongns2611 <jeongns2611@gmail.com>
2026-04-28 14:08:20 +0900

    feat: 프로젝트 생성 관련 Repo 구현

---
+++ BE/src/main/java/com/salmon/studion/domain/project/repository/ProjectRepository.java
@@ -0,0 +1,9 @@
+package com.salmon.studion.domain.project.repository;
+
+import com.salmon.studion.domain.project.entity.Project;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProjectRepository extends JpaRepository<Project, Integer> {
+}

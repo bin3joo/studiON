d8fcbd5e9d55c2f48aa70fa50db283b1f9850b6f
jeongns2611 <jeongns2611@gmail.com>
2026-04-28 17:19:08 +0900

    feat: 프로젝트 목록 조회 API Controller 구현

--- BE/src/main/java/com/salmon/studion/domain/project/controller/ProjectController.java
+++ BE/src/main/java/com/salmon/studion/domain/project/controller/ProjectController.java
@@ -2,6 +2,7 @@ package com.salmon.studion.domain.project.controller;
 
 import com.salmon.studion.domain.project.dto.request.ProjectCreateRequest;
 import com.salmon.studion.domain.project.dto.response.ProjectCreateResponse;
+import com.salmon.studion.domain.project.dto.response.ProjectListResponse;
 import com.salmon.studion.domain.project.facade.ProjectFacade;
 import com.salmon.studion.global.auth.CustomOAuth2User;
 import com.salmon.studion.global.common.response.ApiResponse;
@@ -9,10 +10,7 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/projects")
@@ -21,10 +19,15 @@ public class ProjectController {
 
     private final ProjectFacade projectFacade;
 
+    @GetMapping()
+    public ResponseEntity<ApiResponse<ProjectListResponse>> getProjectList(@AuthenticationPrincipal CustomOAuth2User user) {
+        return ResponseEntity.ok(ApiResponse.success(projectFacade.getProjectList(user.getUserId())));
+    }
+
     @PostMapping()
     public ResponseEntity<ApiResponse<ProjectCreateResponse>> createProject(
-            @RequestBody @Valid ProjectCreateRequest projectCreateRequest
-//            ,@AuthenticationPrincipal CustomOAuth2User user
+            @RequestBody @Valid ProjectCreateRequest projectCreateRequest,
+            @AuthenticationPrincipal CustomOAuth2User user
     ) {
         // TODO: OAuth 구현 후 교체 예정
         Integer userId = 1;

4fce90140f079251476e1860b604c422879445b0
jeongns2611 <jeongns2611@gmail.com>
2026-04-29 16:14:03 +0900

    feat: UserId 기반 User 조회 메서드 추가

--- BE/src/main/java/com/salmon/studion/domain/auth/service/UserService.java
+++ BE/src/main/java/com/salmon/studion/domain/auth/service/UserService.java
@@ -1,8 +1,20 @@
 package com.salmon.studion.domain.auth.service;
 
+import com.salmon.studion.domain.auth.entity.User;
+import com.salmon.studion.domain.auth.repository.UserRepository;
+import com.salmon.studion.global.common.response.ErrorCode;
+import com.salmon.studion.global.exception.BusinessException;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class UserService {
-    
+
+    private final UserRepository userRepository;
+
+    public User getUserByUserId(Integer userId) {
+        return userRepository.findById(userId)
+                .orElseThrow(() -> new BusinessException(ErrorCode.USER_NOT_FOUND));
+    }
 }

0988d40f9e97e7fda69438cc42686d31fe9b445b
한예성 <droppord@naver.com>
2026-04-30 09:59:55 +0900

    fix: NPE 방어로직 추가

--- BE/src/main/java/com/salmon/studion/domain/track/dto/request/TrackAddRequest.java
+++ BE/src/main/java/com/salmon/studion/domain/track/dto/request/TrackAddRequest.java
@@ -1,5 +1,7 @@
 package com.salmon.studion.domain.track.dto.request;
 
+import com.salmon.studion.global.common.response.ErrorCode;
+import com.salmon.studion.global.exception.BusinessException;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -7,8 +9,13 @@ import lombok.Setter;
 @Getter
 @Setter
 @NoArgsConstructor
-public class TrackAddRequest {
-    private Integer projectId;
+public class TrackAddRequest extends TrackRequest{
     private String name;
     private String type;
+
+    @Override
+    public void validate() {
+        if(getProjectId() == null || name == null || type == null)
+            throw new BusinessException(ErrorCode.INVALID_REQUEST);
+    }
 }
--- BE/src/main/java/com/salmon/studion/domain/track/dto/request/TrackReorderRequest.java
+++ BE/src/main/java/com/salmon/studion/domain/track/dto/request/TrackReorderRequest.java
@@ -1,5 +1,7 @@
 package com.salmon.studion.domain.track.dto.request;
 
+import com.salmon.studion.global.common.response.ErrorCode;
+import com.salmon.studion.global.exception.BusinessException;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -7,9 +9,17 @@ import lombok.Setter;
 @Getter
 @Setter
 @NoArgsConstructor
-public class TrackReorderRequest {
-    private Integer projectId;
+public class TrackReorderRequest extends TrackRequest{
     private Integer trackId;
     private Integer targetPreTrackId;
     private Integer targetPostTrackId;
+
+    @Override
+    public void validate() {
+        if(getProjectId() == null || trackId == null)
+            throw new BusinessException(ErrorCode.INVALID_REQUEST);
+
+        if(trackId.equals(targetPreTrackId) || trackId.equals(targetPostTrackId))
+            throw new BusinessException(ErrorCode.INVALID_REQUEST);
+    }
 }

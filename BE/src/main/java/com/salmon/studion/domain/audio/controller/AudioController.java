1800bd35e021b771b46913fb74656bed28302aae
jeongns2611 <jeongns2611@gmail.com>
2026-05-03 22:13:01 +0900

    feat: 오디오 메타데이터 등록 API 구현

--- BE/src/main/java/com/salmon/studion/domain/audio/controller/AudioController.java
+++ BE/src/main/java/com/salmon/studion/domain/audio/controller/AudioController.java
@@ -1,6 +1,8 @@
 package com.salmon.studion.domain.audio.controller;
 
+import com.salmon.studion.domain.audio.dto.request.AudioMetadataCreateRequest;
 import com.salmon.studion.domain.audio.dto.request.AudioUploadUrlRequest;
+import com.salmon.studion.domain.audio.dto.response.AudioMetadataCreateResponse;
 import com.salmon.studion.domain.audio.dto.response.AudioUploadUrlResponse;
 import com.salmon.studion.domain.audio.facade.AudioFacade;
 import com.salmon.studion.global.auth.CustomOAuth2User;
@@ -10,6 +12,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.http.HttpStatus;
 
 @RestController
 @RequestMapping("/api/v1/audios")
@@ -24,5 +27,13 @@ public class AudioController {
             @AuthenticationPrincipal CustomOAuth2User user
     ) {
         return ResponseEntity.ok(ApiResponse.success(audioFacade.getAudioUploadUrl(audioUploadUrlRequest, user.getUserId())));
+    }
+
+    @PostMapping()
+    public ResponseEntity<ApiResponse<AudioMetadataCreateResponse>> createAudioMetadata(
+            @Valid @RequestBody AudioMetadataCreateRequest audioMetadataCreateRequest,
+            @AuthenticationPrincipal CustomOAuth2User user
+    ) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(ApiResponse.success(audioFacade.createAudioMetadata(audioMetadataCreateRequest, user.getUserId())));
     }
 }

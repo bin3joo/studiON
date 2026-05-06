e7bb61d1a54c31ef21d32fbc37d9948d24c59b54
jeongns2611 <jeongns2611@gmail.com>
2026-05-07 07:12:05 +0900

    feat: CommentEventHandler 핸들러 추가

---
+++ BE/src/main/java/com/salmon/studion/global/infrastructure/websocket/handler/CommentEventHandler.java
@@ -0,0 +1,9 @@
+package com.salmon.studion.global.infrastructure.websocket.handler;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class CommentEventHandler {
+}

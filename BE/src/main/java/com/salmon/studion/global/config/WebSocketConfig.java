aa09fb349a8d16cae397f969332b4f17ce7be9b5
jeongns2611 <jeongns2611@gmail.com>
2026-05-06 10:23:25 +0900

    feat: WebsocketConfig에 핸드셰이크 인증 처리 Interceptor 추가

--- BE/src/main/java/com/salmon/studion/global/config/WebSocketConfig.java
+++ BE/src/main/java/com/salmon/studion/global/config/WebSocketConfig.java
@@ -1,7 +1,8 @@
 package com.salmon.studion.global.config;
 
-import com.salmon.studion.global.infrastructure.websocket.ChatWebSocketHandler;
-import com.salmon.studion.global.infrastructure.websocket.ProjectWebSocketHandler;
+import com.salmon.studion.global.infrastructure.websocket.handler.ChatWebSocketHandler;
+import com.salmon.studion.global.infrastructure.websocket.handler.ProjectWebSocketHandler;
+import com.salmon.studion.global.infrastructure.websocket.interceptor.WebSocketHandshakeInterceptor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
@@ -14,11 +15,13 @@ import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry
 public class WebSocketConfig implements WebSocketConfigurer {
 
     private final ProjectWebSocketHandler projectWebSocketHandler;
+    private final WebSocketHandshakeInterceptor webSocketHandshakeInterceptor;
     private final ChatWebSocketHandler chatWebSocketHandler;
 
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
         registry.addHandler(projectWebSocketHandler, "/ws/projects/{projectId}")
+                .addInterceptors(webSocketHandshakeInterceptor)
                 // 개발 환경
                 .setAllowedOrigins("*");
 //                // 운영 환경

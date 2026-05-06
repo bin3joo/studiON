a516586698ae986c20859a2678fd6f19dae928e4
jeongns2611 <jeongns2611@gmail.com>
2026-05-06 16:25:41 +0900

    feat: Project의 웹소켓 이벤트 타입 Enum 구현

--- BE/src/main/java/com/salmon/studion/global/common/enums/ProjectWebSocketEventType.java
+++ BE/src/main/java/com/salmon/studion/global/common/enums/ProjectWebSocketEventType.java
@@ -2,9 +2,17 @@ package com.salmon.studion.global.common.enums;
 
 public enum ProjectWebSocketEventType {
 
-    PROJECT_JOIN,
-    PROJECT_ONLINE_USERS,
-    USER_JOINED_PROJECT;
+    PROJECT_ONLINE_USERS,   // 프로젝트 내 접속자 목록
+
+    PROJECT_JOIN,           // 프로젝트 입장 요청
+    USER_JOINED_PROJECT,    // 프로젝트 입장 브로드캐스트
+
+    PROJECT_LEFT,      // 프로젝트 퇴장 요청
+    USER_LEFT_PROJECT,   // 프로젝트 퇴장 브로드캐스트
+
+    PROJECT_RENAME,         // 프로젝트 이름 수정 요청
+    PROJECT_RENAMED,        // 프로젝트 이름 수정 브로드캐스트
+    ;
 
     public static ProjectWebSocketEventType from(String event) {
         for (ProjectWebSocketEventType value : values()) {

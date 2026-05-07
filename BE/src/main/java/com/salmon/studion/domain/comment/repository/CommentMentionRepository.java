79d16e874eb7b1ecc16e428ca25be70c33a86edd
jeongns2611 <jeongns2611@gmail.com>
2026-05-07 12:42:39 +0900

    feat: CommentMention Repository ID 타입 수정

--- BE/src/main/java/com/salmon/studion/domain/comment/repository/CommentMentionRepository.java
+++ BE/src/main/java/com/salmon/studion/domain/comment/repository/CommentMentionRepository.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
-public interface CommentMentionRepository extends JpaRepository<CommentMention, Integer> {
+public interface CommentMentionRepository extends JpaRepository<CommentMention, CommentMention.CommentMentionId> {
 
     @Query("""
         SELECT cm

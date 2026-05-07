4991155d580f0e3ed4db7c3f428f1ab9b19cdfc8
jeongns2611 <jeongns2611@gmail.com>
2026-05-07 12:36:46 +0900

    feat: CommentDeleteResponse DTO에 필드 추가

--- BE/src/main/java/com/salmon/studion/domain/comment/dto/websocket/CommentDeleteResponse.java
+++ BE/src/main/java/com/salmon/studion/domain/comment/dto/websocket/CommentDeleteResponse.java
@@ -1,5 +1,19 @@
 package com.salmon.studion.domain.comment.dto.websocket;
 
+import com.salmon.studion.domain.comment.entity.Comment;
+
 public record CommentDeleteResponse (
-    Integer commentId
-) {}
+        Integer projectId,
+        Integer trackId,
+        Integer commentId,
+        Integer parentCommentId
+) {
+    public static CommentDeleteResponse of(Integer projectId, Comment comment) {
+        return new CommentDeleteResponse(
+                projectId,
+                comment.getTrack().getId(),
+                comment.getId(),
+                comment.getParentCommentId()
+        );
+    }
+}

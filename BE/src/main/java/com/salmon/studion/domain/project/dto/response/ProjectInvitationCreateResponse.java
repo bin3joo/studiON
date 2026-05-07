e4b13d05ed2d67f097bd00324be98f06496a329d
jeongns2611 <jeongns2611@gmail.com>
2026-05-08 08:26:44 +0900

    feat: 프로젝트 초대코드 발급/등록 관련 DTO에 정적 팩토리 메서드 추가

--- BE/src/main/java/com/salmon/studion/domain/project/dto/response/ProjectInvitationCreateResponse.java
+++ BE/src/main/java/com/salmon/studion/domain/project/dto/response/ProjectInvitationCreateResponse.java
@@ -1,9 +1,23 @@
 package com.salmon.studion.domain.project.dto.response;
 
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
 import java.time.LocalDateTime;
 
+@Getter
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class ProjectInvitationCreateResponse {
     private Integer projectId;
     private String inviteCode;
     private LocalDateTime expiresAt;
+
+    public static ProjectInvitationCreateResponse of(
+            Integer projectId,
+            String inviteCode,
+            LocalDateTime expiresAt
+    ) {
+        return new ProjectInvitationCreateResponse(projectId, inviteCode, expiresAt);
+    }
 }

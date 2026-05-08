71469c11da7b9fade0db7a91c311b3e8b0e0b362
jeongns2611 <jeongns2611@gmail.com>
2026-05-08 13:05:10 +0900

    feat: 초대코드 생성/재발급, 초대코드 조회 Service 메서드 구현

--- BE/src/main/java/com/salmon/studion/domain/project/service/ProjectInviteService.java
+++ BE/src/main/java/com/salmon/studion/domain/project/service/ProjectInviteService.java
@@ -1,11 +1,16 @@
 package com.salmon.studion.domain.project.service;
 
-import com.salmon.studion.domain.project.dto.response.ProjectInvitationAcceptResponse;
+import com.salmon.studion.domain.project.dto.redis.ProjectInviteRedisValue;
 import com.salmon.studion.domain.project.dto.response.ProjectInvitationCreateResponse;
 import com.salmon.studion.domain.project.repository.ProjectInviteRedisRepository;
 import com.salmon.studion.domain.project.support.ProjectInviteCodeGenerator;
+import com.salmon.studion.global.common.response.ErrorCode;
+import com.salmon.studion.global.exception.BusinessException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
 
 @Service
 @RequiredArgsConstructor
@@ -21,11 +26,43 @@ public class ProjectInviteService {
 //    private final ProjectInviteLockManager projectInviteLockManager;
 
     public ProjectInvitationCreateResponse createOrRefreshInvitation(Integer projectId, Integer userId) {
-        return null;
+        projectInviteRedisRepository.deleteActiveInviteByProjectId(projectId);
+
+        String inviteCode = generateUniqueInviteCode();
+        LocalDateTime expiresAt = LocalDateTime.now().plusMinutes(INVITE_CODE_EXPIRATION_MINUTES);
+        Duration ttl = Duration.ofMinutes(INVITE_CODE_TTL_MINUTES);
+
+        ProjectInviteRedisValue value = new ProjectInviteRedisValue(projectId, inviteCode, userId, expiresAt);
+
+        projectInviteRedisRepository.save(value, ttl);
+
+        return ProjectInvitationCreateResponse.of(projectId, inviteCode, expiresAt);
     }
 
+    public ProjectInviteRedisValue getValidInvitation(String inviteCode) {
+        ProjectInviteRedisValue invitation  = projectInviteRedisRepository.findByInviteCode(inviteCode)
+                .orElseThrow(() -> new BusinessException(ErrorCode.PROJECT_INVITE_CODE_INVALID));
 
-    public ProjectInvitationAcceptResponse acceptInvitation(String inviteCode, Integer userId) {
-        return null;
+        if (invitation.isExpired()) {
+            throw new BusinessException(ErrorCode.PROJECT_INVITE_CODE_EXPIRED);
+        }
+
+        return invitation;
+    }
+
+    private String generateUniqueInviteCode() {
+        for (int i=0; i<MAX_INVITE_CODE_GENERATION_RETRY; i++) {
+            String code = projectInviteCodeGenerator.generate();
+            
+            if (!projectInviteRedisRepository.existsByInviteCode(code)) {
+                return code;
+            }
+        }
+
+        throw new BusinessException(ErrorCode.PROJECT_INVITE_CODE_GENERATION_FAILED);
+    }
+
+    public void deleteByInviteCode(String inviteCode) {
+        projectInviteRedisRepository.deleteByInviteCode(inviteCode);
     }
 }

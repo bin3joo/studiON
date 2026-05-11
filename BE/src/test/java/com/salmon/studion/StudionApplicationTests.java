849947c2d48905b690d6622e1fc5a000b0f75b76
seoliee <dev.seoliee49@gmail.com>
2026-05-11 17:37:12 +0900

    fix: test 코드 주석처리 (dev 브랜치에서 빌드 실패 원인)

--- BE/src/test/java/com/salmon/studion/StudionApplicationTests.java
+++ BE/src/test/java/com/salmon/studion/StudionApplicationTests.java
@@ -1,13 +1,12 @@
 package com.salmon.studion;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class StudionApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	// @Test
+	// void contextLoads() {
+	// }
 
 }

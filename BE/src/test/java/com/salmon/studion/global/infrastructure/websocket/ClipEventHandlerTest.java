4955ffc60ee687d9c01a2669bb315a5e190c9577
seoliee <dev.seoliee49@gmail.com>
2026-05-13 14:38:36 +0900

    fix: 테스트코드 주석처리

--- BE/src/test/java/com/salmon/studion/global/infrastructure/websocket/ClipEventHandlerTest.java
+++ BE/src/test/java/com/salmon/studion/global/infrastructure/websocket/ClipEventHandlerTest.java
@@ -1,23 +1,23 @@
-// package com.salmon.studion.global.infrastructure.websocket;
+// // package com.salmon.studion.global.infrastructure.websocket;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.salmon.studion.domain.clip.dto.response.ClipCopyResponse;
-import com.salmon.studion.domain.clip.dto.response.ClipCreateResponse;
-import com.salmon.studion.domain.clip.dto.response.ClipMoveResponse;
-import com.salmon.studion.domain.clip.service.ClipService;
-import com.salmon.studion.global.infrastructure.websocket.common.WsMessage;
-import com.salmon.studion.global.infrastructure.websocket.handler.ClipEventHandler;
-import com.salmon.studion.global.scheduler.ProjectAutosaveScheduler;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Nested;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.Spy;
-import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.web.socket.WebSocketSession;
+// import com.fasterxml.jackson.databind.ObjectMapper;
+// import com.salmon.studion.domain.clip.dto.response.ClipCopyResponse;
+// import com.salmon.studion.domain.clip.dto.response.ClipCreateResponse;
+// import com.salmon.studion.domain.clip.dto.response.ClipMoveResponse;
+// import com.salmon.studion.domain.clip.service.ClipService;
+// import com.salmon.studion.global.infrastructure.websocket.common.WsMessage;
+// import com.salmon.studion.global.infrastructure.websocket.handler.ClipEventHandler;
+// import com.salmon.studion.global.scheduler.ProjectAutosaveScheduler;
+// import org.junit.jupiter.api.BeforeEach;
+// import org.junit.jupiter.api.DisplayName;
+// import org.junit.jupiter.api.Nested;
+// import org.junit.jupiter.api.Test;
+// import org.junit.jupiter.api.extension.ExtendWith;
+// import org.mockito.InjectMocks;
+// import org.mockito.Mock;
+// import org.mockito.Spy;
+// import org.mockito.junit.jupiter.MockitoExtension;
+// import org.springframework.web.socket.WebSocketSession;
 
 // import java.net.URI;
 // import java.util.Map;
@@ -28,10 +28,10 @@ import org.springframework.web.socket.WebSocketSession;
 // @ExtendWith(MockitoExtension.class)
 // class ClipEventHandlerTest {
 
-    @Mock private WebSocketMessageSender webSocketMessageSender;
-    @Mock private ClipService clipService;
-    @Mock private ProjectAutosaveScheduler projectAutosaveScheduler;
-    @Spy  private ObjectMapper objectMapper = new ObjectMapper();
+    // @Mock private WebSocketMessageSender webSocketMessageSender;
+    // @Mock private ClipService clipService;
+    // @Mock private ProjectAutosaveScheduler projectAutosaveScheduler;
+    // @Spy  private ObjectMapper objectMapper = new ObjectMapper();
 
 //     @InjectMocks private ClipEventHandler clipEventHandler;
 

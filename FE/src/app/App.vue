dfb4aae305ffd1fcfce843d2956a786d02b14fd6
hyoseok8948 <hyoseok8948@gmail.com>
2026-05-14 11:15:15 +0900

    feat: 로컬스토리지 이슈때문에 새로고침하면 트랙 날아가는거 해결

--- FE/src/app/App.vue
+++ FE/src/app/App.vue
@@ -1,12 +1,23 @@
 <script setup lang="ts">
-
+import {ref, onMounted} from 'vue'
 import { useAuthStore } from '@/pages/Onboarding/stores/auth.store'
 
 const authStore = useAuthStore()
-// App 로드 시 백그라운드에서 토큰 자동 복구 시도 (Silent Refresh)
-authStore.silentRefresh()
+
+//인증초기화 완료 여부 추적
+const isAuthInitialized = ref(false);
+
+
+onMounted(async () => {
+  // App 로드 시 토큰 자동 복구가 완전히 끝날 때까지 대기
+  await authStore.silentRefresh()
+  
+  // 복구가 끝나면 하위 페이지 렌더링 허용
+  isAuthInitialized.value = true
+})
+
 </script>
 
 <template>
-  <RouterView/>
+  <RouterView v-if="isAuthInitialized" />
 </template>

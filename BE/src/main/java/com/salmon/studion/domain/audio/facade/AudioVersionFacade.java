5c1bf55d1918b8b01ac790b5f52fbdfa63e7d305
jeongns2611 <jeongns2611@gmail.com>
2026-05-18 09:29:29 +0900

    feat: 오디오 버전 생성 후 비동기 렌더 작업 시작 흐름 추가

--- BE/src/main/java/com/salmon/studion/domain/audio/facade/AudioVersionFacade.java
+++ BE/src/main/java/com/salmon/studion/domain/audio/facade/AudioVersionFacade.java
@@ -5,6 +5,7 @@ import com.salmon.studion.domain.audio.dto.response.AudioVersionCreateResponse;
 import com.salmon.studion.domain.audio.dto.response.AudioVersionDeleteResponse;
 import com.salmon.studion.domain.audio.dto.response.AudioVersionDownloadUrlResponse;
 import com.salmon.studion.domain.audio.dto.response.AudioVersionListResponse;
+import com.salmon.studion.domain.audio.render.AudioVersionRenderWorker;
 import com.salmon.studion.domain.audio.service.AudioVersionService;
 import com.salmon.studion.domain.project.service.ProjectMemberService;
 import lombok.RequiredArgsConstructor;
@@ -16,9 +17,14 @@ public class AudioVersionFacade {
 
     private final ProjectMemberService projectMemberService;
     private final AudioVersionService audioVersionService;
+    private final AudioVersionRenderWorker audioVersionRenderWorker;
 
     public AudioVersionCreateResponse createAudioVersion(Integer projectId, AudioVersionCreateRequest request, Integer userId) {
-        return null;
+        projectMemberService.validateProjectMember(projectId, userId);
+
+        AudioVersionCreateResponse response = audioVersionService.createAudioVersion(projectId, request, userId);
+        audioVersionRenderWorker.renderAsync(response.getVersionId());
+        return response;
     }
 
     public AudioVersionListResponse getAudioVersionList(Integer projectId, Integer userId) {

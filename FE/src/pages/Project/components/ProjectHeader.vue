3ff999e8cd4b0d5db302f9e6b0469451f3b5e22e
hyoseok8948 <hyoseok8948@gmail.com>
2026-05-20 13:38:38 +0900

    feat: 추가적인 UX 개선

--- FE/src/pages/Project/components/ProjectHeader.vue
+++ FE/src/pages/Project/components/ProjectHeader.vue
@@ -301,7 +301,6 @@ const isNearLimit = computed(() => {
         @click="emit('open-history')"
       >
         <History class="h-4 w-4" />
-        <span class="absolute right-1.5 top-1.5 h-2 w-2 rounded-full bg-red-500" />
       </button>
 
       <!-- 코멘트 (말풍선 아이콘) -->

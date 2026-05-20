7c1a2c7b52ffd75447e0d0440e2c27d641507c65
유규봉 <rbqhd9709@naver.com>
2026-05-20 12:12:57 +0900

    Dev

--- BE/src/main/java/com/salmon/studion/global/config/WebClientConfig.java
+++ BE/src/main/java/com/salmon/studion/global/config/WebClientConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.client.ExchangeStrategies;
 import org.springframework.web.reactive.function.client.WebClient;
 
 /*
